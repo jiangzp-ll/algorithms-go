000c3d14f450716d9c0fa9558ffa0aba7b5e7fd3
jiangzepeng <1459417758@qq.com>
2020-11-03 15:25:27 +0800

    jiang: add array

---
+++ array/array.go
@@ -0,0 +1,135 @@
+package array
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Array struct {
+	data   []int
+	length int
+}
+
+// NewArray 数组初始化
+func NewArray(cap int) *Array {
+	if cap == 0 {
+		return nil
+	}
+	return &Array{
+		data:   make([]int, cap, cap),
+		length: 0,
+	}
+}
+
+// Len 数组长度
+func (arr *Array) Len() int {
+	return arr.length
+}
+
+// isIndexOutOfRange 判断数组是否越界
+func (arr *Array) isIndexOutOfRange(index int) bool {
+	if index >= cap(arr.data) {
+		return true
+	}
+	return false
+}
+
+// Find 通过索引查找数组，索引范围[0,n-1]
+func (arr *Array) Find(index int) (int, error) {
+	if arr.isIndexOutOfRange(index) {
+		return 0, errors.New("out of index range")
+	}
+	return arr.data[index], nil
+}
+
+// Insert 插入元素
+func (arr *Array) Insert(index, val int) error {
+	if len(arr.data) == arr.Len() {
+		arr.Expansion()
+	}
+	if index != arr.length && arr.isIndexOutOfRange(index) {
+		return errors.New("out of index range")
+	}
+	for i := arr.length; i > index; i-- {
+		arr.data[i] = arr.data[i-1]
+	}
+	arr.data[index] = val
+	arr.length++
+	return nil
+}
+
+// InsertToTail 从尾部插入元素
+func (arr *Array) InsertToTail(val int) error {
+	return arr.Insert(arr.Len(), val)
+}
+
+// Delete 从数组中删除下标是index的元素并返回
+func (arr *Array) Delete(index int) (int, error) {
+	if arr.isIndexOutOfRange(index) {
+		return 0, errors.New("out of index range")
+	}
+	val := arr.data[index]
+	for i := index; i < arr.Len(); i++ {
+		arr.data[i] = arr.data[i+1]
+	}
+	arr.length--
+	return val, nil
+}
+
+// Print 打印数组
+func (arr *Array) Print() {
+	var format string
+	for i := 0; i < arr.Len(); i++ {
+		format += fmt.Sprintf("|%+v", arr.data[i])
+	}
+	fmt.Println(format)
+}
+
+// Expansion 扩容策略
+func (arr *Array) Expansion() {
+	if len(arr.data) < 1024 {
+		newData := make([]int, 2*len(arr.data))
+		for i := 0; i < arr.Len(); i++ {
+			newData[i] = arr.data[i]
+		}
+		arr.data = newData
+	} else {
+		nl := float64(arr.Len()) * 1.25
+		newData := make([]int, int(nl))
+		for i := 0; i < arr.Len(); i++ {
+			newData[i] = arr.data[i]
+		}
+		arr.data = newData
+	}
+}
+
+// MergeArray 合并两个数组成一个有序数组
+func (arr *Array) MergeArray(other *Array) *Array {
+	al := arr.Len()
+	ol := other.Len()
+	result := NewArray(al + ol)
+	var l, r, k int
+	for l < al && r < ol {
+		if arr.data[l] < other.data[r] {
+			_ = result.Insert(k, arr.data[l])
+			l++
+		} else {
+			_ = result.Insert(k, other.data[r])
+			r++
+		}
+		k++
+	}
+	if l < al {
+		for i := l; i < al; i++ {
+			_ = result.Insert(k, arr.data[i])
+			k++
+		}
+	}
+	if r < ol {
+		for i := r; i < ol; i++ {
+			_ = result.Insert(k, other.data[i])
+			k++
+		}
+	}
+	return result
+}
---
+++ array/array_test.go
@@ -0,0 +1,119 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+var arr = NewArray(10)
+
+func TestNewArray(t *testing.T) {
+	cap := 10
+	arr := NewArray(cap)
+	if arr.Len() == 0 && len(arr.data) == 10 {
+		t.Logf("成功初始化长度%d的数组！\n", cap)
+	} else {
+		t.Error("初始化失败！")
+	}
+}
+
+func TestIsIndexOutOfRange(t *testing.T) {
+	if arr.isIndexOutOfRange(5) {
+		t.Error("数组角标越界了")
+	} else {
+		t.Log("数组角标在范围之内")
+	}
+}
+
+func TestArray_Insert(t *testing.T) {
+	err := arr.Insert(0, 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("下标 %d 处，插入了 %d \n", 0, 1)
+}
+
+func TestArray_Find(t *testing.T) {
+	err := arr.Insert(0, 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	result, err := arr.Find(0)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("下标 %d 的元素是 %d \n", 0, result)
+}
+
+func TestArray_InsertToTail(t *testing.T) {
+	for i := 0; i < len(arr.data)-1; i++ {
+		err := arr.Insert(i, i+1)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}
+	err := arr.InsertToTail(100)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	val, err := arr.Find(arr.Len() - 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if val == 100 {
+		t.Logf("数组最后一个元素是 %d \n", val)
+	} else {
+		t.Error("在数组末尾添加元素失败！")
+	}
+}
+
+func TestArray_Delete(t *testing.T) {
+	err := arr.Insert(0, 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	reslut, err := arr.Delete(0)
+	if err != nil {
+		t.Error("delete element failed!")
+		return
+	}
+	t.Logf("从数组中删除了 %d \n", reslut)
+}
+
+func TestArray_Print(t *testing.T) {
+	for i := 0; i <= len(arr.data); i++ {
+		_ = arr.InsertToTail(i + 1)
+	}
+	arr.Print()
+}
+
+// 数组动态扩容
+func TestArray_Expansion(t *testing.T) {
+	array := NewArray(2)
+	fmt.Printf("before array address: %v \n", &array)
+	for i := 0; i < 1025; i++ {
+		_ = array.Insert(i, i+1)
+	}
+	fmt.Printf("after array address: %v \n", &array)
+	fmt.Printf("cap: %d, len: %d \n", cap(array.data), len(array.data))
+	fmt.Println(array.Len())
+	array.Print()
+}
+
+// 将两个有序书组合合并成一个有序数组
+func TestMergeArray(t *testing.T) {
+	arr := NewArray(2)
+	other := NewArray(2)
+	_ = arr.Insert(0, 1)
+	_ = arr.Insert(1, 3)
+	_ = other.Insert(0, 2)
+	_ = other.Insert(1, 4)
+	result := arr.MergeArray(other)
+	fmt.Println(*result)
+}
