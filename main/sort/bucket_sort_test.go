e2ac477389ed0a17eb5e3dbde6259e4403497bb7
jiangzepeng <1459417758@qq.com>
2020-11-16 11:44:35 +0800

    jiang: add bucket sort

---
+++ main/sort/bucket_sort_test.go
@@ -0,0 +1,23 @@
+package sort
+
+import "testing"
+
+func TestBucketSort(t *testing.T) {
+	arr := []int{1, 6, 3, 5, 8, 6, 4}
+	ret := BucketSort(arr)
+	if ret[0] == 1 && ret[len(arr)-1] == 8 {
+		t.Log("桶排序成功")
+	} else {
+		t.Error("桶排序失败")
+	}
+}
+
+func TestBucketSortSimple(t *testing.T) {
+	arr := []int{1, 6, 3, 5, 8, 6, 4}
+	ret := BucketSortSimple(arr)
+	if ret[0] == 1 && ret[len(arr)-1] == 8 {
+		t.Log("桶排序成功")
+	} else {
+		t.Error("桶排序失败")
+	}
+}
