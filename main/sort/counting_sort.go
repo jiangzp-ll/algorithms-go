dd20558756ee5973eeedaf2ec75e85b47760008e
jiangzepeng <1459417758@qq.com>
2020-11-17 11:23:42 +0800

    jiang: update count sort method

--- main/sort/counting_sort.go
+++ main/sort/counting_sort.go
@@ -2,30 +2,11 @@ package sort
 
 // 计数排序
 
-// CountingSort 计数排序
+// CountingSort 计数排序方式一
 func CountingSort(arr []int) []int {
-	length := len(arr)
-	if length <= 1 {
+	if len(arr) <= 1 {
 		return arr
 	}
-	max := getMax(arr)
-	ret := make([]int, max+1)
-	for i := 0; i < length; i++ {
-		ret[arr[i]] += 1
-	}
-	j := 0
-	for i := 0; i < max+1; i++ {
-		for ret[i] > 0 {
-			arr[j] = i
-			j++
-			ret[i]--
-		}
-	}
-	return arr
-}
-
-// CountingSortTwo 计数排序方式二
-func CountingSortTwo(arr []int) []int {
 	// 1. 获取待排序数组的最大值
 	max := getMax(arr)
 	// 2. 构建统计数组
@@ -47,4 +28,26 @@ func CountingSortTwo(arr []int) []int {
 	}
 	return ret
 
+}
+
+// CountingSortTwo 计数排序方式二
+func CountingSortTwo(arr []int) []int {
+	length := len(arr)
+	if length <= 1 {
+		return arr
+	}
+	max := getMax(arr)
+	ret := make([]int, max+1)
+	for i := 0; i < length; i++ {
+		ret[arr[i]] += 1
+	}
+	j := 0
+	for i := 0; i < max+1; i++ {
+		for ret[i] > 0 {
+			arr[j] = i
+			j++
+			ret[i]--
+		}
+	}
+	return arr
 }
