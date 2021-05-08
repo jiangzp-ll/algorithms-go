83a44683681846707028eebeece6447d7c67f1cd
jiangzp <jiangzp.ll@outlook.com>
2021-05-08 17:59:30 +0800

    update array some function

--- data-structure/array/README.md
+++ data-structure/array/README.md
@@ -1,20 +1,20 @@
 ### Array API
-
+数组中暂时只能存放字符串数据，后续继续扩展其他类型数据
 * `func NewArray(cap int) *Array` ：初始化数组
 * `func (arr *Array) Clear()`：清空数组
-* `func (arr *Array) Contains(val interface{}) bool`：判断数组中是否包含该元素
-* `func (arr *Array) Data() []interface{}`：获取数组中所有元素
-* `func (arr *Array) Delete(index int) (interface{}, error)`：删除指定下标的元素
+* `func (arr *Array) Contains(val string) bool`：判断数组中是否包含该元素
+* `func (arr *Array) Data() []string`：获取数组中所有元素
 * `func (arr *Array) Expansion()`：数组扩容
-* `func (arr *Array) Get(index int) (interface{}, error)`：获取指定下标的元素
-* `func (arr *Array) IndexOf(val interface{}) int`：返回元素在数组中的位置 
-* `func (arr *Array) Insert(index int, val interface{}) error`：插入元素
+* `func (arr *Array) Get(index int) (string, error)`：获取指定下标的元素
+* `func (arr *Array) IndexOf(val string) int`：返回元素在数组中的位置 
+* `func (arr *Array) Insert(index int, val string) error`：插入元素
 * `func (arr *Array) IsEmpty() bool`：判断数组是否为空
 * `func (arr *Array) IsIndexOutOfRange(index int) bool`：判断数组下标是否越界
-* `func (arr *Array) Len() int` ：获取数组长度
+* `func (arr *Array) Len() int` ：获取数组元素个数
 * `func (arr *Array) MergeArray(other *Array) *Array`：合并两个数组
-* `func (arr *Array) Replace(index int, val interface{}) error`：替换元素
-* `func (arr *Array) Set(index int, val interface{}) interface{}`：设置指定下标的元素
+* `func (arr *Array) Remove(index int) (string, error)`：删除并返回指定下标的元素
+* `func (arr *Array) Replace(index int, val string) (string, error)`：替换元素并返回旧值
+* `func (arr *Array) Set(index int, val string) error`：设置指定下标的元素
 * `func (arr *Array) Sort()`：数组排序
 * `func (arr *Array) SubArray(start, end int) *Array`：获取子数组
 * `func (arr *Array) ToString() string`：转成用“, ”分隔的字符串
--- data-structure/array/array.go
+++ data-structure/array/array.go
@@ -5,103 +5,58 @@ import (
 	"fmt"
 )
 
-// Array 数组
+// Array ,like Java ArrayList
 type Array struct {
-	data   []int
-	length int
+	data []string
+	len  int
 }
 
-// NewArray 数组初始化
-func NewArray(cap int) *Array {
-	if cap == 0 {
-		return nil
+// NewArray ,init Array
+func NewArray(capacity int) (*Array, error) {
+	if capacity < 0 {
+		return nil, errors.New("capacity must be greater than zero")
 	}
 	return &Array{
-		data:   make([]int, cap, cap),
-		length: 0,
+		data: make([]string, capacity),
+		len:  0,
+	}, nil
+}
+
+// Clear ，make Array to empty
+func (arr *Array) Clear() {
+	for i := 0; i < len(arr.data); i++ {
+		arr.data[i] = ""
 	}
 }
 
-// Len 数组长度
-func (arr *Array) Len() int {
-	return arr.length
-}
-
-// Data 元素
-func (arr *Array) Data() []int {
-	return arr.data
-}
-
-// IsIndexOutOfRange 判断数组是否越界
-func (arr *Array) IsIndexOutOfRange(index int) bool {
-	if index >= cap(arr.data) {
-		return true
+// Contains, determine whether the value is included in the Array
+func (arr *Array) Contains(val string) bool {
+	for i := 0; i < len(arr.data); i++ {
+		if arr.data[i] == val {
+			return true
+		}
 	}
 	return false
 }
 
-// Find 通过索引查找数组，索引范围[0,n-1]
-func (arr *Array) Find(index int) (int, error) {
-	if arr.IsIndexOutOfRange(index) {
-		return 0, errors.New("out of index range")
-	}
-	return arr.data[index], nil
+// Data ,get Array all element
+func (arr *Array) Data() []string {
+	return arr.data
 }
 
-// Insert 插入元素
-func (arr *Array) Insert(index, val int) error {
-	if len(arr.data) == arr.Len() {
-		arr.Expansion()
-	}
-	if index != arr.length && arr.IsIndexOutOfRange(index) {
-		return errors.New("out of index range")
-	}
-	for i := arr.length; i > index; i-- {
-		arr.data[i] = arr.data[i-1]
-	}
-	arr.data[index] = val
-	arr.length++
-	return nil
-}
-
-// InsertToTail 从尾部插入元素
-func (arr *Array) InsertToTail(val int) error {
-	return arr.Insert(arr.Len(), val)
-}
-
-// Delete 从数组中删除下标是index的元素并返回
-func (arr *Array) Delete(index int) (int, error) {
-	if arr.IsIndexOutOfRange(index) {
-		return 0, errors.New("out of index range")
-	}
-	val := arr.data[index]
-	for i := index; i < arr.Len(); i++ {
-		arr.data[i] = arr.data[i+1]
-	}
-	arr.length--
-	return val, nil
-}
-
-// Print 打印数组
-func (arr *Array) Print() {
-	var format string
-	for i := 0; i < arr.Len(); i++ {
-		format += fmt.Sprintf("|%+v", arr.data[i])
-	}
-	fmt.Println(format)
-}
-
-// Expansion 扩容策略
+// Expansion , Array expansion
+// When the number of elements in the array is less than 1024, the capacity becomes twice of the original array;
+// When the number of elements in the array is greater than 1024, the capacity becomes 1.25 times of the original array.
 func (arr *Array) Expansion() {
 	if len(arr.data) < 1024 {
-		newData := make([]int, 2*len(arr.data))
+		newData := make([]string, 2*len(arr.data))
 		for i := 0; i < arr.Len(); i++ {
 			newData[i] = arr.data[i]
 		}
 		arr.data = newData
 	} else {
 		nl := float64(arr.Len()) * 1.25
-		newData := make([]int, int(nl))
+		newData := make([]string, int(nl))
 		for i := 0; i < arr.Len(); i++ {
 			newData[i] = arr.data[i]
 		}
@@ -109,11 +64,61 @@ func (arr *Array) Expansion() {
 	}
 }
 
-// MergeArray 合并两个数组成一个有序数组
+// Get ,get the element at the specified index
+// TODO 全局错误码
+func (arr *Array) Get(index int) (string, error) {
+	if arr.IsIndexOutOfRange(index) {
+		return "", errors.New("out of index range")
+	}
+	return arr.data[index], nil
+}
+
+// IndexOf ,return the index of the value in the Array
+// TODO 全局错误码
+func (arr *Array) IndexOf(val string) int {
+	for i := 0; i < len(arr.data); i++ {
+		if arr.data[i] == val {
+			return i
+		}
+	}
+	return -1
+}
+
+// Insert, insert the value at the specified index
+func (arr *Array) Insert(index int, val string) error {
+	if len(arr.data) == arr.Len() {
+		arr.Expansion()
+	}
+	if index != arr.len && arr.IsIndexOutOfRange(index) {
+		return errors.New("out of index range")
+	}
+	for i := arr.len; i > index; i-- {
+		arr.data[i] = arr.data[i-1]
+	}
+	arr.data[index] = val
+	arr.len++
+	return nil
+}
+
+// IsEmpty ,determine whether the Array is an empty Array
+func (arr *Array) IsEmpty() bool {
+	return arr.len == 0
+}
+
+// IsIndexOutOfRange ,determine whether the index of the Array is out of bounds
+func (arr *Array) IsIndexOutOfRange(index int) bool {
+	return index >= len(arr.data)
+}
+
+// Len , get Array element number
+func (arr *Array) Len() int {
+	return arr.len
+}
+
+// MergeArray , merge two Array
 func (arr *Array) MergeArray(other *Array) *Array {
-	al := arr.Len()
-	ol := other.Len()
-	result := NewArray(al + ol)
+	al, ol := arr.len, other.len
+	result, _ := NewArray(al + ol)
 	var l, r, k int
 	for l < al && r < ol {
 		if arr.data[l] < other.data[r] {
@@ -138,4 +143,58 @@ func (arr *Array) MergeArray(other *Array) *Array {
 		}
 	}
 	return result
+}
+
+// Remove , remove and return the element with the specified index
+func (arr *Array) Remove(index int) (string, error) {
+	if arr.IsIndexOutOfRange(index) {
+		return "", errors.New("out of index range")
+	}
+	val := arr.data[index]
+	for i := index; i < len(arr.data); i++ {
+		arr.data[i] = arr.data[i+1]
+	}
+	arr.len--
+	return val, nil
+}
+
+// Replace ,replace and return the new value with the specified index
+func (arr *Array) Replace(index int, val string) (string, error) {
+	if arr.IsIndexOutOfRange(index) {
+		return "", errors.New("out of index range")
+	}
+	old := arr.data[index]
+	arr.data[index] = val
+	return old, nil
+}
+
+// Set , set a value for the specified index
+func (arr *Array) Set(index int, val string) error {
+	if arr.IsIndexOutOfRange(index) {
+		return errors.New("out of index range")
+	}
+	arr.data[index] = val
+	return nil
+}
+
+// Find 通过索引查找数组，索引范围[0,n-1]
+func (arr *Array) Find(index int) (string, error) {
+	if arr.IsIndexOutOfRange(index) {
+		return "", errors.New("out of index range")
+	}
+	return arr.data[index], nil
+}
+
+// InsertToTail 从尾部插入元素
+func (arr *Array) InsertToTail(val string) error {
+	return arr.Insert(arr.Len(), val)
+}
+
+// Print 打印数组
+func (arr *Array) Print() {
+	var format string
+	for i := 0; i < arr.Len(); i++ {
+		format += fmt.Sprintf("|%+v", arr.data[i])
+	}
+	fmt.Println(format)
 }
