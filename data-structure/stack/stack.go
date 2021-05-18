5af60be448a3c254bca9f118a4317010832a1dac
jiangzp <jiangzp.ll@outlook.com>
2021-05-18 17:06:31 +0800

    add Stack interface

--- data-structure/stack/stack.go
+++ data-structure/stack/stack.go
@@ -1,9 +1,15 @@
 package stack
 
+// Stack , stack interface{}
 type Stack interface {
-	Push(v interface{})
-	Pop() interface{}
-	IsEmpty() bool
-	Top() interface{}
+	// Flush ,clear the stack
 	Flush()
+	// IsEmpty ,determine whether the stack is empty
+	IsEmpty() bool
+	// Pop , pop the element from the top of the stack
+	Pop() interface{}
+	// Push ,push the element to top of the stack
+	Push(v interface{})
+	// Top , get top of the stack
+	Top() interface{}
 }
