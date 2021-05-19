f8e83ce3e183e02a7c2b2eaef33a3ca5d1119201
jiangzp <jiangzp.ll@outlook.com>
2021-05-20 00:53:38 +0800

    refactor ArrayStack unit test

--- data-structure/stack/stack_base_on_array_test.go
+++ data-structure/stack/stack_base_on_array_test.go
@@ -6,42 +6,42 @@ import (
 	"testing"
 )
 
-var stack = NewArrayStack()
+var as = NewArrayStack()
 
 func Test_ArrayStack_Flush(t *testing.T) {
 	for i := 1; i <= 5; i++ {
-		stack.Push(i)
+		as.Push(i)
 	}
-	stack.Flush()
-	if stack.IsEmpty() {
-		t.Log("stack flush is success")
+	as.Flush()
+	if as.IsEmpty() {
+		t.Log("as flush is success")
 	} else {
-		t.Error("stack flush is failed")
+		t.Error("as flush is failed")
 	}
 }
 
 func Test_ArrayStack_IsEmpty(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	for i := 1; i <= 5; i++ {
-		stack.Push(i)
+		as.Push(i)
 	}
-	if !stack.IsEmpty() {
-		t.Log("stack is not empty")
+	if !as.IsEmpty() {
+		t.Log("as is not empty")
 	} else {
 		t.Error("function IsEmpty has bug")
 	}
 }
 
 func Test_ArrayStack_Peek(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	elements := []int{1, 2, 3}
-	pushValueToStack(elements)
-	ret, err := stack.Peek()
+	pushValueToArrayStack(elements)
+	ret, err := as.Peek()
 	if err != nil {
 		t.Errorf("peek top has error! error: %v \n", err)
 		return
 	}
-	if ret == elements[len(elements)-1] && len(stack.data) == len(elements) {
+	if ret == elements[len(elements)-1] && len(as.data) == len(elements) {
 		t.Log("peek top is success")
 	} else {
 		t.Error("peek top is failed")
@@ -49,10 +49,10 @@ func Test_ArrayStack_Peek(t *testing.T) {
 }
 
 func Test_ArrayStack_Peek_With_Stack_Is_Empty(t *testing.T) {
-	_, err := stack.Peek()
+	_, err := as.Peek()
 	if err != nil {
 		if errors.Is(err, errors2.StackIsEmptyError) {
-			t.Log("stack is empty")
+			t.Log("as is empty")
 			return
 		} else {
 			t.Errorf("unknown error! error: %v \n", err)
@@ -64,15 +64,15 @@ func Test_ArrayStack_Peek_With_Stack_Is_Empty(t *testing.T) {
 }
 
 func Test_ArrayStack_Pop(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	elements := []int{1, 2, 3}
-	pushValueToStack(elements)
-	ret, err := stack.Pop()
+	pushValueToArrayStack(elements)
+	ret, err := as.Pop()
 	if err != nil {
 		t.Errorf("pop the top has error! error: %v \n", err)
 		return
 	}
-	if ret == elements[len(elements)-1] && len(stack.data) == len(elements)-1 {
+	if ret == elements[len(elements)-1] && len(as.data) == len(elements)-1 {
 		t.Log("pop the top is success")
 	} else {
 		t.Error("pop the top is failed")
@@ -80,10 +80,10 @@ func Test_ArrayStack_Pop(t *testing.T) {
 }
 
 func Test_ArrayStack_Pop_With_Stack_Is_Empty(t *testing.T) {
-	_, err := stack.Pop()
+	_, err := as.Pop()
 	if err != nil {
 		if errors.Is(err, errors2.StackIsEmptyError) {
-			t.Log("stack is empty")
+			t.Log("as is empty")
 			return
 		} else {
 			t.Errorf("unknown error! error: %v \n", err)
@@ -95,58 +95,58 @@ func Test_ArrayStack_Pop_With_Stack_Is_Empty(t *testing.T) {
 }
 
 func Test_ArrayStack_Push(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	flag := true
 	elements := []int{1, 2, 3}
-	pushValueToStack(elements)
-	for i := 0; i < len(stack.data); i++ {
-		if stack.data[i] != elements[i] {
+	pushValueToArrayStack(elements)
+	for i := 0; i < len(as.data); i++ {
+		if as.data[i] != elements[i] {
 			flag = false
 			break
 		}
 	}
-	if !stack.IsEmpty() && flag && cap(stack.data) == BasicCapacity {
-		t.Log("push element to stack is success")
+	if !as.IsEmpty() && flag && cap(as.data) == BasicCapacity {
+		t.Log("push element to as is success")
 	} else {
-		t.Error("push element to stack is failed")
+		t.Error("push element to as is failed")
 	}
 }
 
 func Test_ArrayStack_Push_With_Stack_Is_Full(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	var elements []int
 	for i := 1; i < 20; i++ {
 		elements = append(elements, i)
 	}
-	pushValueToStack(elements)
-	if !stack.IsEmpty() && cap(stack.data) == BasicCapacity*2 {
-		t.Log("push element to stack is success")
+	pushValueToArrayStack(elements)
+	if !as.IsEmpty() && cap(as.data) == BasicCapacity*2 {
+		t.Log("push element to as is success")
 	} else {
-		t.Error("push element to stack is failed")
+		t.Error("push element to as is failed")
 	}
 }
 
 func Test_ArrayStack_Push_With_Stack_Is_Full_And_Number_Greater_Than_1024(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	var elements []int
 	for i := 1; i < 1026; i++ {
 		elements = append(elements, i)
 	}
 	newCap := int(float64(len(elements)-1) * 1.25)
-	pushValueToStack(elements)
-	if !stack.IsEmpty() && cap(stack.data) == newCap {
-		t.Log("push element to stack is success")
+	pushValueToArrayStack(elements)
+	if !as.IsEmpty() && cap(as.data) == newCap {
+		t.Log("push element to as is success")
 	} else {
-		t.Error("push element to stack is failed")
+		t.Error("push element to as is failed")
 	}
 }
 
 func Test_ArrayStack_Search(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	target := 2
 	elements := []int{1, target, 3}
-	pushValueToStack(elements)
-	ret, err := stack.Search(target)
+	pushValueToArrayStack(elements)
+	ret, err := as.Search(target)
 	if err != nil {
 		t.Errorf("search the value has error! error: %v \n", err)
 		return
@@ -159,10 +159,10 @@ func Test_ArrayStack_Search(t *testing.T) {
 }
 
 func Test_ArrayStack_Search_With_Stack_Is_Empty(t *testing.T) {
-	_, err := stack.Search(1)
+	_, err := as.Search(1)
 	if err != nil {
 		if errors.Is(err, errors2.StackIsEmptyError) {
-			t.Log("stack is empty")
+			t.Log("as is empty")
 			return
 		} else {
 			t.Errorf("unknown error! error: %v \n", err)
@@ -174,13 +174,13 @@ func Test_ArrayStack_Search_With_Stack_Is_Empty(t *testing.T) {
 }
 
 func Test_ArrayStack_Search_With_The_Value_Not_Exist(t *testing.T) {
-	defer stack.Flush()
+	defer as.Flush()
 	elements := []int{1, 2, 3}
-	pushValueToStack(elements)
-	_, err := stack.Search("a")
+	pushValueToArrayStack(elements)
+	_, err := as.Search("a")
 	if err != nil {
 		if errors.Is(err, errors2.NotExistError) {
-			t.Log("stack is empty")
+			t.Log("as is empty")
 			return
 		} else {
 			t.Errorf("unknown error! error: %v \n", err)
@@ -191,9 +191,9 @@ func Test_ArrayStack_Search_With_The_Value_Not_Exist(t *testing.T) {
 	}
 }
 
-// pushValueToStack .
-func pushValueToStack(elements []int) {
+// pushValueToArrayStack .
+func pushValueToArrayStack(elements []int) {
 	for _, e := range elements {
-		stack.Push(e)
+		as.Push(e)
 	}
 }
